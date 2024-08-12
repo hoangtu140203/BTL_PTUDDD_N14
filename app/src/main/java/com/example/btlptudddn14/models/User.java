16314371ed6d3f1e865753167c0e1bab3f8dbd2f
Hoang Anh Tu <tuhap140203@gmail.com>
2024-08-12 18:02:03 +0700

    completemodels

---
+++ app/src/main/java/com/example/btlptudddn14/models/User.java
@@ -0,0 +1,128 @@
+package com.example.btlptudddn14.models;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class User {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private static User instance;
+    private User() {
+
+    }
+    private User(int id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+    public static synchronized User getInstance() {
+        if (instance == null) {
+            instance = new User();
+        }
+        return instance;
+    }
+    public void initialize(int id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+    // Ham hash code dung de luu mat khau
+    public  String hashPassword(String password) {
+        try {
+            // Tạo đối tượng MessageDigest với thuật toán SHA-256
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // Chuyển đổi mật khẩu thành mảng byte
+            byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+
+            // Chuyển đổi mảng byte thành chuỗi hex
+            StringBuilder hexString = new StringBuilder(2 * encodedHash.length);
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Xử lý nếu thuật toán không tồn tại
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public boolean isPasswordValid(String password) {
+        // Kiểm tra mật khẩu có ít nhất 5 ký tự
+        if (password.length() < 5) {
+            return false;
+        }
+
+        // Kiểm tra mật khẩu chứa ít nhất một chữ cái hoa
+        Pattern uppercasePattern = Pattern.compile("[A-Z]");
+        Matcher uppercaseMatcher = uppercasePattern.matcher(password);
+        if (!uppercaseMatcher.find()) {
+            return false;
+        }
+
+        // Kiểm tra mật khẩu chứa ít nhất một số
+        Pattern digitPattern = Pattern.compile("[0-9]");
+        Matcher digitMatcher = digitPattern.matcher(password);
+        if (!digitMatcher.find()) {
+            return false;
+        }
+        return true;
+    }
+    public void destroy(){
+        instance = null;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
